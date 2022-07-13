a499a9e1b488a46e8b75c23e069b749ea3273c26
KAYITARES <cyntkayitare@example.com>
2022-07-13 10:45:25 +0200

    remove findone on application.js

--- routes/application.js
+++ routes/application.js
@@ -5,23 +5,22 @@ const sendEmail = require("../utils/mail");
 router.post("/apply", async (req, res) => {
   try {
     // console.log(req.body);
-    const isApplicantExist = await Application.findOne({
-      email: req.body.email.trim(),
-    });
-    if (isApplicantExist) {
-      return res.status(202).json({
-        message: `Hey! ${isApplicantExist?.firstName}, your application has been arleady received succesfully`,
-      });
-    } else {
-      const savedApplication = await Application.create(req.body);
-      // sendEmail(req.body);
-      console.log(` ---------------------${savedApplication}`);
+    // const isApplicantExist = await Application.findOne({
+    //   email: req.body.email.trim(),
+    // });
+    // if (isApplicantExist) {
+    //   return res.status(202).json({
+    //     message: `Hey! ${isApplicantExist?.firstName} your application has been arleady received succesfully,  Now schedule for interview`,
+    //   });
+    // } else {
+    const savedApplication = await Application.create(req.body);
+    // sendEmail(req.body);
 
-      return res.status(200).json({
-        message: `Thank you! ${isApplicantExist?.firstName}, your application has been received successfuly`,
-        data: savedApplication,
-      });
-    }
+    return res.status(200).json({
+      message: `your application has been receive.We shall communicate the next step via email`,
+
+      data: savedApplication,
+    });
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -38,7 +37,6 @@ router.get("/all", async (req, res) => {
     return res.status(500).json(err);
   }
 });
-
 router.delete("/:id", async (req, res) => {
   try {
     const user = await Application.findByIdAndDelete(req.params.id);
